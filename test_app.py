d355634bb26ec854f39a44aa695afa1f68d89195
unknown <bsef18a018@pucit.edu.pk>
2021-12-13 09:33:25 +0500

    Pytest-cov testing on python code(Fibonacci sequence and factorial function)

---
+++ test_app.py
@@ -0,0 +1,22 @@
+
+import unittest
+from app import fibonacci
+from app import factorial
+
+
+class TestFibonacci(unittest.TestCase):
+    def test_fibonacci_1(self):
+        self.assertEqual(fibonacci(1), 1)
+    
+    def test_fibonacci_10(self):
+        self.assertEqual(fibonacci(10), 89)
+
+    def test_fibonacci_30(self):
+        self.assertEqual(fibonacci(30), 1346269)
+
+class TestFactorial(unittest.TestCase):
+    def test_factorial_1(self):
+        self.assertEqual(factorial(1), 1)
+
+# if __name__ == '__main__':
+#     unittest.main()
